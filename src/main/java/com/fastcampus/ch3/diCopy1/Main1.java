91141af32634f3d8ec3e49eb741b5223c5cd0b83
kiziri <58429073+kiziri@users.noreply.github.com>
2022-04-28 00:28:34 +0900

    SpringDI 흉내내기 실습 - 프로퍼티로 기능제공 코드의 분리 연습

--- src/main/java/com/fastcampus/ch3/diCopy1/Main1.java
+++ src/main/java/com/fastcampus/ch3/diCopy1/Main1.java
@@ -6,13 +6,26 @@ import java.util.Properties;
 class Car{}
 class SuperCar extends Car {}
 class Truck extends Car {}
+class Engine {}
 
 public class Main1 {
     public static void main(String[] args) throws Exception {
         Car car = new SuperCar();
         Car car1 = getCar();
+        Engine engine = (Engine) getObject("engine");
         System.out.println("car = " + car);
         System.out.println("car = " + car1);
+        System.out.println("car = " + engine);
+    }
+
+    // key를 입력받아 해당하는 value의 객채를 생성하도록함
+    static Object getObject(String key) throws Exception {
+        Properties p = new Properties();
+        p.load(new FileReader("config.txt"));
+
+        Class clazz = Class.forName(p.getProperty(key));
+
+        return clazz.newInstance();
     }
 
     static Car getCar() throws Exception {

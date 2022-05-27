cab507e2b34294e8636a4a8b286e7b5440416e2f
kiziri <58429073+kiziri@users.noreply.github.com>
2022-05-28 01:40:26 +0900

    SpringDI 활용하기 - spring jdbc 연결 및 간단 TDD 연습

---
+++ src/test/java/com/fastcampus/ch3/DBConnectionTest2Test.java
@@ -0,0 +1,23 @@
+package com.fastcampus.ch3;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+import static org.junit.Assert.*;
+
+public class DBConnectionTest2Test {
+    @Test
+    public void springJdbcConnectionTest() throws Exception {
+        ApplicationContext ac = new GenericXmlApplicationContext("file:src/main/webapp/WEB-INF/spring/**/root-context.xml");
+        DataSource ds = ac.getBean(DataSource.class);
+
+        Connection conn = ds.getConnection(); // 데이터베이스의 연결을 얻는다.
+
+        System.out.println("conn = " + conn);
+        //assertTrue(conn!=null);
+    }
+}

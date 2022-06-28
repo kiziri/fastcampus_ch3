dbd77d4e47d54c2ccb5b0a7f2a159488d207c777
kiziri <58429073+kiziri@users.noreply.github.com>
2022-06-29 01:44:10 +0900

    Spring Transaction - propagation 속성, REQUIRES_NEW 구현 및 테스트 실습

--- src/main/java/com/fastcampus/ch3/TxService.java
+++ src/main/java/com/fastcampus/ch3/TxService.java
@@ -1,26 +1,60 @@
 package com.fastcampus.ch3;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import javax.sql.DataSource;
 
 @Service
 public class TxService {
     @Autowired A1Dao a1Dao;
     @Autowired B1Dao b1Dao;
 
-    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+    @Autowired DataSource ds;
+
+//    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
     public void insertA1WithTx() throws Exception {
-        a1Dao.insert(1, 100);   // 성공
-        insertB1WithTx();
-        a1Dao.insert(2, 200);   // 성공
+        PlatformTransactionManager tm = new DataSourceTransactionManager(ds);
+        DefaultTransactionDefinition txd = new DefaultTransactionDefinition();
+        txd.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+        TransactionStatus status = tm.getTransaction(txd);
+        // Tx 시작
+        try {
+            a1Dao.insert(1, 100);   // 성공
+            insertB1WithTx();
+            a1Dao.insert(2, 200);   // 성공
+            tm.commit(status);
+        } catch (Exception e) {
+            e.printStackTrace();
+            tm.rollback(status);
+        } finally {
+        }
     }
 
-    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+    @Transactional(propagation = Propagation.REQUIRES_NEW, rollbackFor = Exception.class)
     public void insertB1WithTx() throws Exception {
-        b1Dao.insert(1, 100);   // 성공
-        b1Dao.insert(1, 200);   // 실패
+        PlatformTransactionManager tm = new DataSourceTransactionManager(ds);
+        DefaultTransactionDefinition txd = new DefaultTransactionDefinition();
+        txd.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+        TransactionStatus status = tm.getTransaction(txd);
+
+        // Tx 시작
+        try {
+            b1Dao.insert(1, 100);   // 성공
+            b1Dao.insert(1, 200);   // 실패
+            tm.commit(status);
+        } catch (Exception e) {
+            e.printStackTrace();
+            tm.rollback(status);
+        } finally {
+        }
     }
 
     public void insertA1WithoutTx() throws Exception {
